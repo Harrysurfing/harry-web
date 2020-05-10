4c3badd9ae43adf15e0ccbcfdea69a395dbdf635
Harrysurfing <harrwangok@gmail.com>
2020-05-10 18:44:13 +1000

    fixed mobile version

--- src/components/flip-card/flip-card.component.jsx
+++ src/components/flip-card/flip-card.component.jsx
@@ -12,7 +12,7 @@ function FlipCard({ title, place, time, details }) {
 					<span className="title">{title}</span>
 					<span className="time">{time}</span>
 				</div>
-				<div onMouseOut={() => setFlipped(!flipped)} className="card-back">
+				<div onClick={() => setFlipped(!flipped)} className="card-back">
 					<ul className="list">
 						{details.map((detail, idx) => {
 							return <li key={idx}>{detail}</li>;
--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,8 +1,36 @@
 import React, { Component } from "react";
 import "./navbar.style.css";
 import { withRouter } from "react-router-dom";
+import ReorderIcon from "@material-ui/icons/Reorder";
 
 export class Navbar extends Component {
+	constructor(props) {
+		super(props);
+
+		const width = document.body.clientWidth;
+
+		this.state = {
+			showNav: width < 768 ? false : true,
+		};
+	}
+
+	handleClick = () => {
+		this.setState({ showNav: !this.state.showNav });
+	};
+
+	componentDidMount() {
+		window.addEventListener("resize", this.handleResize);
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener("resize", this.handleResize);
+	}
+
+	handleResize = () => {
+		let newWidth = document.body.clientWidth;
+		this.setState({ showNav: newWidth < 768 ? false : true });
+	};
+
 	render() {
 		const { history, location } = this.props;
 		const isHome = location.pathname === "/" ? true : false;
@@ -16,17 +44,30 @@ export class Navbar extends Component {
 		const dynamicClass = `${isHome ? "button-black" : "button-white"}`;
 
 		return (
-			<div className="nav-container" style={styles.container}>
-				<span onClick={() => history.push("/")} className={dynamicClass}>
-					home
-				</span>
-				<span onClick={() => history.push("/resume")} className={dynamicClass}>
-					resume
-				</span>
-				<span onClick={() => history.push("/contact")} className={dynamicClass}>
-					contact
-				</span>
-			</div>
+			<React.Fragment>
+				<div className="mobile-button" onClick={this.handleClick}>
+					<ReorderIcon fontSize="large" />
+				</div>
+				{this.state.showNav ? (
+					<div className="nav-container" style={styles.container}>
+						<span onClick={() => history.push("/")} className={dynamicClass}>
+							home
+						</span>
+						<span
+							onClick={() => history.push("/resume")}
+							className={dynamicClass}
+						>
+							resume
+						</span>
+						<span
+							onClick={() => history.push("/contact")}
+							className={dynamicClass}
+						>
+							contact
+						</span>
+					</div>
+				) : null}
+			</React.Fragment>
 		);
 	}
 }
